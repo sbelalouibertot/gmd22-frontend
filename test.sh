d154070b7c3a322c60cd7574e81d18ece96ff0ec
Samy Belaloui-Bertot <samy.belaloui@bellman.immo>
2022-04-24 12:27:46 +0200

    fix: removed test file

--- test.sh
+++
@@ -1,9 +0,0 @@
-ssh pi@77.192.114.110 "
-cd /home/pi/Documents/gmd-22/gmd22-backend
-sudo nohup sh -c '
-pwd
-var=OUI
-echo bonjour
-echo $var
-' > /home/pi/Documents/gmd-22/gmd22-backend/logs/test.txt 2>&1 &
-"
